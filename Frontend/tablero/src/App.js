5ba5f695db898d95134ae2458131df235e8d899e
Camilo Hernandez M <camilo.hernandez2020@alu.uct.cl>
2022-05-18 22:52:31 -0400

    av front

--- Frontend/tablero/src/App.js
+++ Frontend/tablero/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+import logo from './chessBoard.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className = "izquierda">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenido ChessOnline de LongCastling
         </p>
         <a
           className="App-link"
@@ -15,8 +16,14 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React hola 
+          Learn React
         </a>
+        </div>
+        <div className = "derecha">
+
+      TODO DERECHA
+
+        </div>
       </header>
     </div>
   );
